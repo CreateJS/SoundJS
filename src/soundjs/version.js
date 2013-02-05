005e3591284e9e386e06edc86a3dd301355c154b
Lanny McNie <lanny@gskinner.com>
2013-02-05 14:38:27 -0700

    Updated docs and lib.

--- src/soundjs/version.js
+++ src/soundjs/version.js
@@ -1,28 +1,28 @@
-(function() {
+(function () {
 
-/**
- * Static class holding library specific information such as the version and buildDate of the library.
- *  The SoundJS class has been renamed {{#crossLink "Sound"}}{{/crossLink}}.  Please see {{#crossLink "Sound"}}{{/crossLink}}
- *  for information on using sound.
- * @class SoundJS
- **/
-var o = this.createjs = this.createjs||{};
-o = (o.SoundJS = o.SoundJS||{});
- 
-/**
- * The version string for this release.
- * @property version
- * @type String
- * @static
- **/
-o.version = /*version*/"NEXT"; // injected by build process
+	/**
+	 * Static class holding library specific information such as the version and buildDate of the library.
+	 *  The SoundJS class has been renamed {{#crossLink "Sound"}}{{/crossLink}}.  Please see {{#crossLink "Sound"}}{{/crossLink}}
+	 *  for information on using sound.
+	 * @class SoundJS
+	 **/
+	var o = this.createjs = this.createjs || {};
+	o = (o.SoundJS = o.SoundJS || {});
 
-/**
- * The build date for this release in UTC format.
- * @property buildDate
- * @type String
- * @static
- **/
-o.buildDate = /*date*/"Fri, 25 Jan 2013 21:13:57 GMT"; // injected by build process
+	/**
+	 * The version string for this release.
+	 * @property version
+	 * @type String
+	 * @static
+	 **/
+	o.version = /*version*/"NEXT"; // injected by build process
+
+	/**
+	 * The build date for this release in UTC format.
+	 * @property buildDate
+	 * @type String
+	 * @static
+	 **/
+	o.buildDate = /*date*/"Tue, 05 Feb 2013 21:37:02 GMT"; // injected by build process
 
 })();
