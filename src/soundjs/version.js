4a7913f4a6fe6f867eeff5705686363287b68a21
Lanny McNie <lanny@gskinner.com>
2013-05-09 15:57:22 -0600

    Updated libs & docs.
    Signed-off-by: Lanny McNie <lanny@gskinner.com>

--- src/soundjs/version.js
+++ src/soundjs/version.js
@@ -27,6 +27,6 @@
 	 * @type String
 	 * @static
 	 **/
-	o.buildDate = /*date*/"Mon, 29 Apr 2013 17:26:13 GMT"; // injected by build process
+	o.buildDate = /*date*/"Thu, 09 May 2013 21:56:21 GMT"; // injected by build process
 
 })();
