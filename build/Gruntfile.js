68222f0b5a9da854c38027057548486324ff5081
Lanny McNie <lanny@gskinner.com>
2013-09-10 14:19:51 -0600

    Updated the build process to use NodeJS & Grunt.js.
    Please refer to the readme in the build folder.
    
    Signed-off-by: Lanny McNie <lanny@gskinner.com>

---
+++ build/Gruntfile.js
@@ -0,0 +1,165 @@
+var path = require('path');
+var _ = require('lodash');
+
+module.exports = function (grunt) {
+	grunt.initConfig(
+			{
+				pkg: grunt.file.readJSON('package.json'),
+
+				// Default values
+				version: 'NEXT',
+				name: 'soundjs',
+				docsZip: "<%= pkg.name %>_docs-<%= version %>.zip",
+
+				// Setup doc names / paths.
+				docsName: '<%= pkg.name %>_docs-<%= version %>',
+				docsZip: "<%= docsName %>.zip",
+				docsFolder: "./output/<%= docsName %>/",
+
+				// Setup Uglify for JS minification.
+				uglify: {
+					options: {
+						banner: grunt.file.read('LICENSE'),
+						preserveComments: "some",
+						compress: {
+							global_defs: {
+								"DEBUG": false
+							}
+						},
+					},
+					build: {
+						files: {
+							'output/<%= pkg.name.toLowerCase() %>-<%= version %>.min.js': getConfigValue('source'),
+							'output/flashplugin-<%= version %>.min.js': getConfigValue('flashplugin_source'),
+						}
+					}
+				},
+
+				// Build docs using yuidoc
+				yuidoc: {
+					compile: {
+						name: '<%= pkg.name %>',
+						version: '<%= version %>',
+						description: '<%= pkg.description %>',
+						url: '<%= pkg.url %>',
+						logo: '<%= pkg.logo %>',
+						options: {
+							paths: ['../src/'],
+							outdir: '<%= docsFolder %>',
+							linkNatives: true,
+							attributesEmit: true,
+							selleck: true,
+							helpers: ["./path.js"],
+							themedir: "createjsTheme/"
+						}
+					}
+				},
+
+				compress: {
+					build: {
+						options: {
+							mode:'zip',
+							archive:'output/<%= docsZip %>'
+						},
+						files: [
+							{expand:true, src:'**', cwd:'<%= docsFolder %>'}
+						]
+					}
+				},
+
+				copy: {
+					docsZip: {
+						files: [
+							{expand:true, cwd:'output/', src:'<%= docsZip %>', dest:'../docs/'}
+						]
+					},
+					docsSite: {
+						files: [
+							{expand:true, cwd:'<%= docsFolder %>', src:'**', dest:getConfigValue('docs_out_path')}
+						]
+					},
+					src: {
+						files: [
+							{expand: true, cwd:'output/', src: '*.js', dest: '../lib/'}
+						]
+					}
+				},
+
+				updateversion: {
+					build: {
+						file: '../src/soundjs/version.js',
+						version: '<%= version %>'
+					},
+				}
+			}
+	);
+
+	function getBuildConfig() {
+		// Read the global settings file first.
+		var config = grunt.file.readJSON('config.json');
+
+		// If we have a config.local.json .. prefer its values.
+		if (grunt.file.exists('config.local.json')) {
+			var config2 = grunt.file.readJSON('config.local.json');
+			_.extend(config, config2);
+		}
+
+		return config;
+	}
+
+	function getConfigValue(name) {
+		var config = grunt.config.get('buildConfig');
+
+		if (config == null) {
+			config = getBuildConfig();
+			grunt.config.set('buildConfig', config);
+		}
+
+		return config[name];
+	}
+
+	// Load all the tasks we need
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-yuidoc');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadTasks('tasks/');
+
+	/**
+	 * Build the docs using YUIdocs.
+	 */
+	grunt.registerTask('docs', [
+		"yuidoc", "compress", "copy:docsZip"
+	]);
+
+	/**
+	 * Sets out version to the version in package.json (defaults to NEXT)
+	 */
+	grunt.registerTask('setVersion', function () {
+		grunt.config.set('version', grunt.config.get('pkg').version);
+	});
+
+	/**
+	 * Task for exporting a next build.
+	 *
+	 */
+	grunt.registerTask('next', [
+		"coreBuild"
+	]);
+
+	/**
+	 * Task for exporting a release build (version based on package.json)
+	 *
+	 */
+	grunt.registerTask('build', [
+		"setVersion", "coreBuild", "copy:docsSite"
+	]);
+
+	/**
+	 * Main build task, always runs after next or build.
+	 *
+	 */
+	grunt.registerTask('coreBuild', [
+		"updateversion", "uglify", "docs", "copy:src"
+	]);
+};
