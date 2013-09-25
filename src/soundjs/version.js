47522e08ae81fe2c8c8e6cefa127538537d2ba44
Lanny McNie <lanny@gskinner.com>
2013-09-25 11:18:39 -0600

    SoundJS version 0.5.0
    Signed-off-by: Lanny McNie <lanny@gskinner.com>

--- src/soundjs/version.js
+++ src/soundjs/version.js
@@ -19,7 +19,7 @@ this.createjs = this.createjs || {};
 	 * @type String
 	 * @static
 	 **/
-	s.version = /*version*/"NEXT"; // injected by build process
+	s.version = /*version*/"0.5.0"; // injected by build process
 
 	/**
 	 * The build date for this release in UTC format.
@@ -27,6 +27,6 @@ this.createjs = this.createjs || {};
 	 * @type String
 	 * @static
 	 **/
-	s.buildDate = /*date*/"Thu, 12 Sep 2013 21:45:41 GMT"; // injected by build process
+	s.buildDate = /*date*/"Wed, 25 Sep 2013 17:09:35 GMT"; // injected by build process
 
 })();
