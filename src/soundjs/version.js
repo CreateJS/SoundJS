995942e0338f2e39eb9cc6b398af36dd468bef5f
Lanny McNie <lanny@gskinner.com>
2013-12-12 15:19:38 -0700

    Updated to latest EventDispatcher. Pushed latest VERSIONS
    Signed-off-by: Lanny McNie <lanny@gskinner.com>

--- src/soundjs/version.js
+++ src/soundjs/version.js
@@ -27,6 +27,6 @@ this.createjs = this.createjs || {};
 	 * @type String
 	 * @static
 	 **/
-	s.buildDate = /*date*/"Wed, 27 Nov 2013 19:49:18 GMT"; // injected by build process
+	s.buildDate = /*date*/"Thu, 12 Dec 2013 22:18:59 GMT"; // injected by build process
 
 })();
