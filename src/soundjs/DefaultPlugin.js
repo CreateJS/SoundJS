275e1d8d37de2c932646d329e24d296dc230f016
OJay Robinson <ojay@gskinner.com>
2014-10-31 10:46:02 -0600

    start work on DefaultPlugin and DefaultSountInstance

---
+++ src/soundjs/DefaultPlugin.js
@@ -0,0 +1,244 @@
+/*
+ * DefaultPlugin
+ * Visit http://createjs.com/ for documentation, updates and examples.
+ *
+ *
+ * Copyright (c) 2012 gskinner.com, inc.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// namespace:
+this.createjs = this.createjs || {};
+
+(function () {
+	"use strict";
+	/**
+	 * A default plugin class used as a base for all other plugins.
+	 * @class DefaultPlugin
+	 * @constructor
+	 * @since 0.5.3
+	 */
+
+	var DefaultPlugin = function () {
+		this._init();
+	};
+	var p = DefaultPlugin.prototype;
+	DefaultPlugin.prototype.constructor = DefaultPlugin;
+
+	/**
+	 * The capabilities of the plugin. This is generated via the {{#crossLink "DefaultPlugin/_generateCapabilities:method"}}{{/crossLink}}
+	 * method and is used internally.
+	 * @property _capabilities
+	 * @type {Object}
+	 * @default null
+	 * @protected
+	 * @static
+	 */
+	p._capabilities = null;
+
+	/**
+	 * Object hash indexed by the source URI of each file to indicate if an audio source has begun loading,
+	 * is currently loading, or has completed loading.  Can be used to store non boolean data after loading
+	 * is complete (for example arrayBuffers for web audio).
+	 * @property _audioSources
+	 * @type {Object}
+	 * @protected
+	 */
+	p._audioSources = null;
+
+	/**
+	 * The internal master volume value of the plugin.
+	 * @property _volume
+	 * @type {Number}
+	 * @default 1
+	 * @protected
+	 */
+	p._volume = 1;
+
+	/**
+	 * A reference to a loader class used by a plugin that must be set.
+	 * @type {Object}
+	 * @protected
+	 */
+	p._loader;
+
+	/**
+	 * A reference to a SoundInstance class used by a plugin that must be set.
+	 * @type {Object}
+	 * @protected;
+	 */
+	p._soundInstance;
+
+	/**
+	 * An initialization function run by the constructor
+	 * @method _init
+	 * @protected
+	 */
+	p._init = function () {
+		this._capabilities = s._capabilities;
+		this._audioSources = {};
+	};
+
+	/**
+	 * Pre-register a sound for preloading and setup. This is called by {{#crossLink "Sound"}}{{/crossLink}}.
+	 * Note all plugins provide a <code>Loader</code> instance, which <a href="http://preloadjs.com" target="_blank">PreloadJS</a>
+	 * can use to assist with preloading.
+	 * @method register
+	 * @param {String} src The source of the audio
+	 * @param {Number} instances The number of concurrently playing instances to allow for the channel at any time.
+	 * Note that not every plugin will manage this value.
+	 * @return {Object} A result object, containing a "tag" for preloading purposes.
+	 */
+	p.register = function (src, instances) {
+		this._audioSources[src] = true;
+		if (!this._loader) {return;}
+		var loader = {tag: new this._loader(src, this)};
+		return loader;
+	};
+
+	/**
+	 * Checks if preloading has started for a specific source. If the source is found, we can assume it is loading,
+	 * or has already finished loading.
+	 * @method isPreloadStarted
+	 * @param {String} src The sound URI to check.
+	 * @return {Boolean}
+	 */
+	p.isPreloadStarted = function (src) {
+		return (this._audioSources[src] != null);
+	};
+
+	/**
+	 * Checks if preloading has finished for a specific source.
+	 * @method isPreloadComplete
+	 * @param {String} src The sound URI to load.
+	 * @return {Boolean}
+	 */
+	p.isPreloadComplete = function (src) {
+		return (!(this._audioSources[src] == null || this._audioSources[src] == true));
+	};
+
+	/**
+	 * Remove a sound added using {{#crossLink "WebAudioPlugin/register"}}{{/crossLink}}. Note this does not cancel a preload.
+	 * @method removeSound
+	 * @param {String} src The sound URI to unload.
+	 */
+	p.removeSound = function (src) {
+		delete(this._audioSources[src]);
+	};
+
+	/**
+	 * Remove all sounds added using {{#crossLink "WebAudioPlugin/register"}}{{/crossLink}}. Note this does not cancel a preload.
+	 * @method removeAllSounds
+	 * @param {String} src The sound URI to unload.
+	 */
+	p.removeAllSounds = function () {
+		this._audioSources = {};
+	};
+
+	/**
+	 * Handles internal preload completion.
+	 * @method _handlePreloadComplete
+	 * @protected
+	 */
+	p._handlePreloadComplete = function (loader) {
+		createjs.Sound._sendFileLoadEvent(loader.src);
+		loader.cleanUp();
+	};
+
+	/**
+	 * Internally preload a sound.
+	 * @method preload
+	 * @param {String} src The sound URI to load.
+	 * @param {Object} tag An HTML audio tag used to load src.
+	 */
+	p.preload = function (src, tag) {
+		this._audioSources[src] = true;
+		if (!this._loader) {return;}
+		var loader = new this._loader(src, this);
+		loader.onload = this._handlePreloadComplete;
+		loader.load();
+	};
+
+	/**
+	 * Create a sound instance. If the sound has not been preloaded, it is internally preloaded here.
+	 * @method create
+	 * @param {String} src The sound source to use.
+	 * @param {Number} startTime Audio sprite property used to apply an offset, in milliseconds.
+	 * @param {Number} duration Audio sprite property used to set the time the clip plays for, in milliseconds.
+	 * @return {SoundInstance} A sound instance for playback and control.
+	 */
+	p.create = function (src, startTime, duration) {
+		if (!this.isPreloadStarted(src)) {this.preload(src);}
+		return new this._soundInstance(src, startTime, duration, this);
+	};
+
+	/**
+	 * Set the master volume of the plugin, which affects all SoundInstances.
+	 * @method setVolume
+	 * @param {Number} value The volume to set, between 0 and 1.
+	 * @return {Boolean} If the plugin processes the setVolume call (true). The Sound class will affect all the
+	 * instances manually otherwise.
+	 */
+	p.setVolume = function (value) {
+		this._volume = value;
+		this._updateVolume();
+		return true;
+	};
+
+	/**
+	 * Set the gain value for master audio. Should not be called externally.
+	 * @method _updateVolume
+	 * @protected
+	 */
+	p._updateVolume = function () {
+		// Plugin Specific code
+	};
+
+	/**
+	 * Get the master volume of the plugin, which affects all SoundInstances.
+	 * @method getVolume
+	 * @return The volume level, between 0 and 1.
+	 */
+	p.getVolume = function () {
+		return this._volume;
+	};
+
+	/**
+	 * Mute all sounds via the plugin.
+	 * @method setMute
+	 * @param {Boolean} value If all sound should be muted or not. Note that plugin-level muting just looks up
+	 * the mute value of Sound {{#crossLink "Sound/getMute"}}{{/crossLink}}, so this property is not used here.
+	 * @return {Boolean} If the mute call succeeds.
+	 */
+	p.setMute = function (value) {
+		this._updateVolume();
+		return true;
+	};
+
+	p.toString = function () {
+		return "[DefaultAudioPlugin]";
+	};
+
+	createjs.DefaultPlugin = DefaultPlugin;
+
+}());
