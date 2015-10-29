bb406cb764bc1d2c666b53e42bfcde34d2211738
Lanny McNie <lanny@gskinner.com>
2015-10-28 21:02:16 -0600

    Fixed incorrect usage of on() in both code and docs.

--- src/soundjs/Sound.js
+++ src/soundjs/Sound.js
@@ -109,12 +109,12 @@ this.createjs = this.createjs || {};
 	 *      createjs.FlashAudioPlugin.swfPath = "../src/soundjs/flashaudio";
 	 *      createjs.Sound.registerPlugins([createjs.WebAudioPlugin, createjs.FlashAudioPlugin]);
 	 *      createjs.Sound.alternateExtensions = ["mp3"];
-	 *      createjs.Sound.on("fileload", createjs.proxy(this.loadHandler, (this)));
+	 *      createjs.Sound.on("fileload", this.loadHandler, this);
 	 *      createjs.Sound.registerSound("path/to/mySound.ogg", "sound");
 	 *      function loadHandler(event) {
      *          // This is fired for each sound that is registered.
      *          var instance = createjs.Sound.play("sound");  // play using id.  Could also use full source path or event.src.
-     *          instance.on("complete", createjs.proxy(this.handleComplete, this));
+     *          instance.on("complete", this.handleComplete, this);
      *          instance.volume = 0.5;
 	 *      }
 	 *
