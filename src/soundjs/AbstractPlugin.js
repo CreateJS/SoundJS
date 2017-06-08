373099338423e6a15d88fdc7c77c54c5e092bd93
Lanny McNie <lannymcnie@users.noreply.github.com>
2017-06-08 11:21:17 -0600

    Fixed issue where mute/volume were not properly set on new instances (only affected HTML audio)

--- src/soundjs/AbstractPlugin.js
+++ src/soundjs/AbstractPlugin.js
@@ -227,6 +227,11 @@ this.createjs = this.createjs || {};
 			this._soundInstances[src].push(si);
 		}
 
+		// Plugins that don't have a setVolume should implement a setMasterVolune/setMasterMute
+		// So we have to check that here.
+		si.setMasterVolume && si.setMasterVolume(createjs.Sound.volume);
+		si.setMasterMute && si.setMasterMute(createjs.Sound.muted);
+
 		return si;
 	};
 
